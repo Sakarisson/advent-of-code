f08e8679f8f2c7fd9b29931d7d1c0fa4aa157bb2
Kristian Sakarisson <kristian.sakarisson@wolt.com>
2018-12-07 00:23:29 +0200

    comparison

--- Day 3/part 1/index.nim
+++ Day 3/part 1/index.nim
@@ -23,17 +23,37 @@ proc formatClaim(rawData: string): Claim =
   var
     rawId = exploded[0]
     id = parseInt(rawId[1 .. rawId.len - 1])
-  echo id
+    rawTopLeft = exploded[2]
+    topLeftX = parseInt(rawTopLeft.split(',')[0])
+    rawTopLeftY = rawTopLeft.split(',')[1]
+    topLeftY = parseInt(rawTopLeftY[0 .. rawTopLeftY.len - 2])
+    topLeft = Coordinate(x: topLeftX, y: topLeftY)
+    translation = exploded[3].split('x')
+    translateX = parseInt(translation[0])
+    translateY = parseInt(translation[1])
+    bottomRightX = topLeftX + translateX - 1
+    bottomRightY = topLeftY + translateY - 1
+    bottomRight = Coordinate(x: bottomRightX, y: bottomRightY)
+
   Claim(
-    id: 123,
-    topLeft: Coordinate(x: 3, y: 7),
-    bottomRight: Coordinate(x: 5, y: -3)
+    id: id,
+    topLeft: topLeft,
+    bottomRight: bottomRight
   )
+
+proc doOverlap(a: Claim, b: Claim) : bool =
+  if a.topLeft.x >= b.bottomRight.x or a.topLeft.x >= b.bottomRight.x:
+    return false
+  if a.topLeft.y <= b.bottomRight.y or a.topLeft.y <= b.bottomRight.y:
+    return false
+  return true
 
 var formatted = map(stringInput, formatClaim)
 
 for claim in formatted:
   echo claim
+
+echo doOverlap(formatted[0], formatted[2])
 
 # import sequtils
 
